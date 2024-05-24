8bb5fb97445435799d051891fbf5493a58c3a22f
Ugine <makutin_ea@mail.ru>
2024-05-24 22:59:05 +0300

    Изменил код на загрузку  весов Llama 3

--- src/download_model.py
+++ src/download_model.py
@@ -1,4 +1,4 @@
 from transformers import pipeline
 from huggingface_hub import snapshot_download
 
-model = snapshot_download("microsoft/Phi-3-small-8k-instruct", cache_dir="weights")
+model = snapshot_download("NousResearch/Meta-Llama-3-8B-Instruct", cache_dir="weights")
