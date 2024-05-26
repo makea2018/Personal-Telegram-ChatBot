97f72bfff049ed028017310f3d61d4f19a72c0f2
Ugine <makutin_ea@mail.ru>
2024-05-26 21:38:12 +0300

    Проверка ответов бота в двух разных ролях

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ from chatbots.saiga_chatbot import SaigaBot
 import json
 
 # Загрузка истории чата
-chat_history = [
-    {"role": "system", "content": "Ты - умный чат бот, который отвечает максимально четко и по делу на вопросы пользователя. Ответы ты даешь на том языке, на котором с тобой общается пользователь."},
-]
+with open('chat_history.json', 'r', encoding='utf-8') as f:
+    chat_history = json.load(f)
 
 # Инициализация чат-бота
-bot = SaigaBot("weights/Saiga-Llama-3-8B", chat_history)
+bot = SaigaBot("weights/Saiga-Llama-3-8B")
 
 # Вопрос боту
-answer = bot.generate_answer("Привет, расскажи о себе. Чем ты отличаешь от обычной Llama-3-8B от компании Meta?")
+answer = bot.generate_answer("Кто играл роль капитана Джека Воробья в Пиратах Карибского моря?")
+
+print("Ответ обычный: " + answer)
+
+# Меняем роль бота
+bot.change_role("Ты - бывалый моряк, в своих ответах ты используешь морские слова: палундра, якорь мне в бухту и другие в этом роде.")
+
+# Вопрос боту
+answer = bot.generate_answer("Кто играл роль капитана Джека Воробья в Пиратах Карибского моря?")
 
 # Сохранение истории чата
-bot.save_chat_history()
+# bot.save_chat_history()
 
-print(answer)
+print("Ответ в роли моряка: " + answer)
