70d29106f16a41fd492c7e8da055400538de7a99
Ugine <makutin_ea@mail.ru>
2024-05-30 23:50:07 +0300

    Добавил загрузку сети kandinsky 2.2

--- src/download_diffusion_model.py
+++ src/download_diffusion_model.py
@@ -1,4 +1,8 @@
-from huggingface_hub import snapshot_download
+from huggingface_hub import snapshot_download, hf_hub_download
 
 # Загрузка модели SD-XL 1.0-base
-midjourney_v6 = snapshot_download("stabilityai/stable-diffusion-xl-base-1.0", cache_dir="weights")
+# sd_xl_1_0_base = snapshot_download("stabilityai/stable-diffusion-xl-base-1.0", cache_dir="weights")
+
+# Загрузка модели kandinsky-2-2-prior
+kandinsky_2_2_prior = snapshot_download("kandinsky-community/kandinsky-2-2-prior",
+                                        cache_dir="weights")
